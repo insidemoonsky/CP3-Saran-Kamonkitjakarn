ec07abd209290ff7f7ecfccf99210dab12dee4f8
insidemoonsky <70510858+insidemoonsky@users.noreply.github.com>
2021-02-23 17:11:06 +0700

    Add files via upload

---
+++ Exercise5_1_Saran_K.py
@@ -0,0 +1,12 @@
+num1 = int(input("Number1 : "))
+num2 = int(input("Number2 : "))
+num_plus = num1+num2
+num_minus = num1-num2
+num_multi = num1*num2
+num_div = num1/num2
+
+print(num1,"+",num2,"=",num_plus)
+print(num1,"-",num2,"=",num_minus)
+print(num1,"*",num2,"=",num_multi)
+print(num1,"/",num2,"=",num_div)
+
---
+++ Exercise5_2_Saran_K.py
@@ -0,0 +1,5 @@
+s = int(input("S : " ))
+t = int(input("T : " ))
+v = s/t
+
+print(v ,"km/h")
