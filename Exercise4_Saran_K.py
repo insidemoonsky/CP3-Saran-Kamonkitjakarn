83798f3f541c7a8a892e1919d115edc57b9ff050
insidemoonsky <70510858+insidemoonsky@users.noreply.github.com>
2021-02-23 11:46:07 +0700

    Add files via upload

---
+++ Exercise4_Saran_K.py
@@ -0,0 +1,12 @@
+f_eng = 27.0
+g_bus = 25.0
+i_cs = 30.5
+c_prg = 29.75
+
+print("--- Your Score ---")
+print("Foundation English :",f_eng)
+print("General Business :",g_bus)
+print("Introduction to Computer Systems :",i_cs)
+print("Computer Programming :",c_prg)
+
+
